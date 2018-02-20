cdc6790c739a838c201923d02a4a1f841e5234af
Seongjin, Park <Seongjin, Park@HM5F0664>
2018-02-20 18:39:13 +0900

    first sample

---
+++ spring5webapp/src/main/java/seongjin/springframework/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,11 @@
+package seongjin.springframework.spring5webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import seongjin.springframework.spring5webapp.model.Book;
+
+/**
+ * Created by jt on 5/16/17.
+ */
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
