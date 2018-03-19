ad7dc28456fc68540b0eaaf8dd1c44c3e7a11b60
Seongjin, Park <Seongjin, Park@HM5F0664>
2018-03-19 21:18:27 +0900

    di-demo init

---
+++ spring5webapp/src/main/java/seongjin/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,29 @@
+package seongjin.springframework.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import seongjin.springframework.spring5webapp.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+
+	private AuthorRepository authorRepository;
+	
+	
+	public AuthorController(AuthorRepository authorRepository) {
+		this.authorRepository = authorRepository;
+	}
+
+
+	@RequestMapping("/authors")
+	public String getBooks(Model model) {
+		
+		model.addAttribute("authors", authorRepository.findAll());
+		
+		return "authors";
+		
+	}
+	
+}
